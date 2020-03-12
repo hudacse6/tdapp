407702ccf4c6cea5ada3ca0b824c93c50bc55b49
hudacse6 <hudacse6@gmail.com>
2020-03-12 11:24:53 +0600

    forth commit

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('add_todo/', views.add_todo, name='add_todo'),
     path('delete_todo/<int:todo_id>/', views.delete_todo, name='delete_todo')
-    # ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 ]
